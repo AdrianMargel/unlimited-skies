d31606a9817cdb059a3c07ff5f5092ad01221d7b
AdrianMargel <carbongrouplife@gmail.com>
2023-07-24 17:08:06 -0600

    partially reenable shader updates + background

--- client/play/core/game.js
+++ client/play/core/game.js
@@ -440,10 +440,15 @@ class Game{
 
 		if(sharedTextures.isLoaded()){
 			// let clouds=this.getClouds();
+			if(!this.started){
+				this.cloudSave=this.getClouds();
+			}
+			let clouds=this.cloudSave;
+			this.started=this.started??true;
 
-			// sharedTextures.run(disp.cam,this.waterLine.line,this.shadowLine.line,clouds);
-			// cloudPaint.run(disp.cam,clouds,this.time,this.screenStart);
-			// background.run(disp.cam,this.cloudSeedOffset,this.time,this.screenStart);
+			sharedTextures.run(disp.cam,this.waterLine.line,this.shadowLine.line,clouds);
+			cloudPaint.run(disp.cam,clouds,this.time,this.screenStart);
+			background.run(disp.cam,this.cloudSeedOffset,this.time,this.screenStart);
 			// bulletRenderer.run(disp.cam,this.screenStart);
 			// renderer.run(disp.cam,this.screenStart);
 			// particleRenderer.run(disp.cam);
