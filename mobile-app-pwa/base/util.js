7cdec5499f30cbf7a05f9c5b272fb7316f0b9d86
AdrianMargel <carbongrouplife@gmail.com>
2023-07-27 17:31:52 -0600

    cloned mobile app

---
+++ mobile-app-pwa/base/util.js
@@ -0,0 +1,116 @@
+class WeakRefSet{
+	constructor(){
+		//TODO: consider using a weakSet internally along side an array to speed up lookups
+		this.array=[];
+	}
+	
+	has(toFind){
+		for(let item of this){
+			if(item===toFind){
+				return true;
+			}
+		}
+		return false;
+	}
+	add(toAdd){
+		if(!this.has(toAdd)){
+			this.array.push(new WeakRef(toAdd));
+			return true;
+		}
+		return false;
+	}
+	delete(toDelete){
+		let idx=this.#findIdx(toDelete);
+		if(idx!=-1){
+			this.array.splice(idx,1);
+			return true;
+		}
+		return false;
+	}
+	clear(){
+		this.array=[];
+	}
+
+	forEach(func){
+		for(let item of this) {
+			func(item);
+		}
+	}
+
+	#findIdx(toFind){
+		let idx=0;
+		//the list may shorten while iterating but the idx will always be correct since purged items are not returned by the iterator
+		for(let item of this) {
+			if(item===toFind){
+				return idx;
+			}
+			idx++;
+		}
+		return -1;
+	}
+	*[Symbol.iterator](){
+		for(let i=0;i<this.array.length;i++){
+			let item=this.array[i].deref();
+			if(item===undefined){
+				this.array.splice(i,1);
+				i--;
+			}else{
+				yield item;
+			}
+		}
+	}
+}
+function loopVec(vec,ref){
+	let modP=vec.cln();
+	let mapWidth=gameRunner.getMapSize().x;
+	let offsetEdge=ref.x-mapWidth/2;
+	modP.x=mod(modP.x-offsetEdge,mapWidth)+offsetEdge;
+	return modP;
+}
+function runAndFilter(arr,func){
+	for(let i=arr.length-1;i>=0;i--){
+		if(!func(arr[i])){
+			arr.splice(i,1);
+		}
+	}
+}
+function binarySearch(arr, el, compare_fn) {
+    let m = 0;
+    let n = arr.length - 1;
+    while (m <= n) {
+        let k = (n + m) >> 1;
+        let cmp = compare_fn(el, arr[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return ~m;
+}
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+function bezierPoint(t, p0, p1, p2, p3){
+	let cX = 3 * (p1.x - p0.x),
+		bX = 3 * (p2.x - p1.x) - cX,
+		aX = p3.x - p0.x - cX - bX;
+
+	let cY = 3 * (p1.y - p0.y),
+		bY = 3 * (p2.y - p1.y) - cY,
+		aY = p3.y - p0.y - cY - bY;
+
+	let x = (aX * Math.pow(t, 3)) + (bX * Math.pow(t, 2)) + (cX * t) + p0.x;
+	let y = (aY * Math.pow(t, 3)) + (bY * Math.pow(t, 2)) + (cY * t) + p0.y;
+
+	return new Vector(x,y);
+}
---
+++ mobile-app-pwa/definitions/sounds.js
@@ -0,0 +1,282 @@
+class Sound{
+	constructor(manager,url,volume=1,maxSounds=4){
+		this.url=url;
+		this.manager=manager;
+		this.volume=volume;
+		this.buffer=null;
+		this.ceilingStart=8000;
+		this.ceilingEnd=12000;
+
+		this.startedCount=0;
+		this.maxSounds=maxSounds;//This is new sounds per frame, not concurrently playing sounds
+	}
+	init(){
+		this.load(this.url);
+		return this;
+	}
+	async load(url){
+		const response=await fetch(url);
+		this.manager.ctx.decodeAudioData(await response.arrayBuffer()).then((decodedData)=>{
+			this.buffer=decodedData;
+		});
+	}
+	prime(){
+		this.startedCount=0;
+	}
+	getChance(){
+		let space=this.maxSounds-this.startedCount;
+		if(space>this.maxSounds/2){
+			return 1;
+		}else{
+			return space/(this.maxSounds/2);
+		}
+	}
+	play(p=null,timeOffset=0,speed=1,volume=1,force=false){
+		if(this.buffer==null||((this.manager.paused||this.manager.muted)&&!force)){
+			return;
+		}
+		if(!force&&this.getChance()<Math.random()){
+			return;
+		}
+		let playerP=gameRunner.getPlayer().getPos(p);
+		p=p??playerP;
+		let dist=p.mag(playerP);
+		let distVol=clamp(1-dist/8000,0,1);
+		if(distVol==0){
+			return;
+		}
+		let alt=-p.y;
+		let spaceVol=clamp((this.ceilingEnd-alt)/(this.ceilingEnd-this.ceilingStart),0,1);
+
+		let context=this.manager.ctx;
+
+		let source=context.createBufferSource();
+		source.buffer=this.buffer;
+		source.connect(context.destination);
+
+		source.playbackRate.value=speed;
+
+		let gainNode = context.createGain();
+		if(force){
+			gainNode.gain.value=(volume)-1;
+		}else{
+			gainNode.gain.value=(this.manager.volume*this.volume*volume*distVol*spaceVol)-1;
+		}
+
+		gainNode.connect(context.destination);
+		source.connect(gainNode);
+
+		let timeGap=.01;//without this chrome makes odd clicking sounds occasionally
+		source.start(context.currentTime+timeOffset+timeGap);
+		this.startedCount++;
+	}
+}
+//Note that looping sounds are always running, but they can be silenced
+class SoundLooping extends Sound{
+	constructor(manager,url,volume=1,speed=1){
+		super(manager,url,volume);
+		this.speed=speed;
+		this.source=null;
+		this.gainNode=null;
+		this.isPlaying=false;
+
+		this.t1=0;
+		this.t2=0;
+		this.vol1=-1;
+		this.vol2=-1;
+	}
+	async load(url){
+		const response=await fetch(url);
+		this.manager.ctx.decodeAudioData(await response.arrayBuffer()).then((decodedData)=>{
+			this.buffer=decodedData;
+			this.source=this.manager.ctx.createBufferSource();
+			this.source.buffer=this.buffer;
+			this.source.connect(this.manager.ctx.destination);
+
+			this.source.loop=true;
+			this.source.playbackRate.value=this.speed;
+
+			this.gainNode=this.manager.ctx.createGain();
+			this.source.connect(this.gainNode);
+			this.gainNode.connect(this.manager.ctx.destination);
+			this.gainNode.gain.setValueAtTime(-1,0);
+			this.gainNode.gain.value=-1;
+		});
+	}
+	play(p,timeOffset=0,speed=1,volume=1){
+		if(this.source==null||this.manager.paused||this.manager.muted){
+			return;
+		}
+		if(!this.isPlaying){
+			this.isPlaying=true;
+			this.source.start(this.manager.ctx.currentTime);
+		}
+		if(speed!=-1){
+			this.source.playbackRate.value=this.speed=speed;
+		}
+		this.pushVolume(timeOffset,volume*this.volume*this.manager.volume-1);
+		this.gainNode.gain.setValueAtTime(this.vol1,this.t1);
+		this.gainNode.gain.linearRampToValueAtTime(this.vol2,this.t2);
+	}
+	pushVolume(timeOffset,volume){
+		this.vol1=this.getVolume(this.manager.ctx.currentTime);
+		this.vol2=volume;
+
+		this.t1=this.manager.ctx.currentTime;
+		this.t2=this.manager.ctx.currentTime+timeOffset;
+	}
+	getVolume(time){
+		if(this.t2<=this.t1){
+			return this.vol1;
+		}
+		let mixT=clamp((time-this.t1)/(this.t2-this.t1),0,1);
+		return mix(this.vol1,this.vol2,mixT);
+	}
+	// Do not stop, stopped sounds cannot be restarted
+	// stop(){
+	// 	if(this.isPlaying){
+	// 		this.isPlaying=false;
+	// 		this.source.stop();
+	// 	}
+	// }
+}
+class SoundManager{
+	constructor(){
+		this.volumeBase=0;
+		this.volume=0;
+		this.muted=true;
+		this.paused=false;
+		this.ctx=new AudioContext();
+
+		this.sounds=[
+			this.gunshot=new Sound(this,"/sound/gunshot.mp3",1).init(),
+			this.hit=new Sound(this,"/sound/hit.mp3",5).init(),
+			this.bang=new Sound(this,"/sound/bang.mp3",1).init(),
+			this.laser=new Sound(this,"/sound/laser.mp3",.75).init(),
+			this.splash=new Sound(this,"/sound/splash-2.mp3",5).init(),
+		];
+		this.rocket=new SoundLooping(this,"/sound/rocket.mp3",.75).init();
+		this.prop=new SoundLooping(this,"/sound/prop.mp3",1).init();
+	}
+	prime(){
+		this.sounds.forEach(s=>s.prime());
+	}
+	pause(){
+		this.rocket.play(null,.5,-1,0);
+		this.prop.play(null,.5,-1,0);
+		this.paused=true;
+	}
+	resume(){
+		this.paused=false;
+	}
+	setVolume(vol){
+		this.volumeBase=vol;
+		if(!this.muted){
+			this.volume=vol;
+		}
+	}
+	mute(){
+		this.rocket.play(null,.5,-1,0);
+		this.prop.play(null,.5,-1,0);
+		this.muted=true;
+		this.volume=0;
+	}
+	unmute(){
+		this.muted=false;
+		this.volume=this.volumeBase;
+	}
+}
+class MusicManager{
+	constructor(){
+		this.volumeBase=0;
+		this.volume=0;
+		this.muted=true;
+		this.queueNext=true;
+
+		this.audioPlayer=document.createElement("audio");
+		this.audioPlayer.addEventListener("ended",()=>this.nextSong());
+
+		this.songs=[
+			"Geometrical Dominator.mp3",
+			"Rocket Race.mp3",
+			"Clutterfunk, Pt. 2.mp3",
+			"Beatfever.mp3",
+			"Fireburst.mp3",
+			"Give Me a Break.mp3",
+			"Snow in the Air.mp3",
+			"Run.mp3",
+			"Striker.mp3",
+			"Combo Breaker.mp3",
+			"Supra Zone.mp3",
+			"Backbonebreaks.mp3",
+		];
+		//note that this is a reference, not a copy
+		this.songsBackup=this.songs;
+		this.shuffle();
+	}
+	shuffle(){
+		shuffle(this.songs);
+	}
+	forcePlaylist(urls){
+		this.songs=urls;
+		this.nextSong();
+	}
+	restorePlaylist(){
+		this.songs=this.songsBackup;
+		this.nextSong();
+	}
+	nextSong(){
+		if(this.muted){
+			this.queueNext=true;
+			return;
+		}
+		let song=this.songs[0];
+		this.audioPlayer.setAttribute("src","/music/"+song);
+		this.audioPlayer.currentTime=0;
+		this.audioPlayer.volume=this.volume;
+		this.audioPlayer.play();
+
+		//remove the song and insert it randomly into the second half of the list
+		let first=this.songs.splice(0,1)[0];
+		let insertI=Math.floor(random(this.songs.length/2,this.songs.length));
+		this.songs.splice(insertI,0,first);
+	}
+	setVolume(vol){
+		this.volumeBase=vol;
+		if(!this.muted){
+			if(vol==0){
+				this.audioPlayer.pause();
+			}else if(this.volume==0){
+				if(this.queueNext){
+					this.nextSong();
+					this.queueNext=false;
+				}else{
+					this.audioPlayer.play();
+				}
+			}
+			this.volume=vol;
+			this.audioPlayer.volume=vol;
+		}
+	}
+	mute(){
+		this.muted=true;
+		this.audioPlayer.pause();
+
+		this.volume=0;
+		this.audioPlayer.volume=this.volume;
+	}
+	unmute(){
+		this.muted=false;
+		if(this.volumeBase!=0){
+			if(this.queueNext){
+				this.nextSong();
+				this.queueNext=false;
+			}else{
+				this.audioPlayer.play();
+			}
+		}
+
+		this.volume=this.volumeBase;
+		this.audioPlayer.volume=this.volume;
+	}
+}
