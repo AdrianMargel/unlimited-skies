63f044a5bbefb3c0936ea5d244a05772dfba4fd2
AdrianMargel <carbongrouplife@gmail.com>
2023-07-28 00:52:58 -0600

    add support for app subdomain

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import { Application, send } from "https://deno.land/x/oak/mod.ts";
 
-const ROOT_DIR=Deno.cwd()+"/client"
+const ROOT_DIR=Deno.cwd()+"/client";
+const APP_ROOT_DIR=Deno.cwd()+"/mobile-app-pwa";
 
 const app=new Application();
 app.use(async (ctx, next)=>{
 	const filePath=ctx.request.url.pathname;
+	let root;
+	if(ctx.request.url.hostname.split(".")[0].toLocaleLowerCase()=="app"){
+		root=APP_ROOT_DIR;
+	}else{
+		root=ROOT_DIR;
+	}
 	try{
 		await send(ctx,filePath,{
-			root: ROOT_DIR,
+			root,
 			index: "index.html",
 		});
 	}catch{
