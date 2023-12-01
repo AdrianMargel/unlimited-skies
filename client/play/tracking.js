16029b4a92c9ab343d99bd2f37957e431e5265c0
AdrianMargel <carbongrouplife@gmail.com>
2023-12-01 10:10:45 -0700

    dont track localhost

--- client/play/tracking.js
+++ client/play/tracking.js
@@ -1,4 +1,10 @@
 function track(type,info){
+	let host=window.location.hostname;
+	if(host.toLowerCase()=="localhost"||host=="127.0.0.1"){
+		// Don't bother tracking localhost
+		return;
+	}
+	
 	let url=window.location.pathname;
 	fetch("/tracks",{
 		body:JSON.stringify({
